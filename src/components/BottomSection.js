4e0dd59e2f017f2add382b8375149da450078889
Lo <hundsimma@gmail.com>
2019-01-11 15:49:15 +0100

    Add email

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import preval from 'preval.macro'
 
 import BaseText from './reusables/BaseText'
@@ -8,12 +8,15 @@ import BaseLink from './reusables/BaseLink'
 export default () => {
   const time = preval`module.exports = require('date-fns').format(new Date(), 'D MMMM YYYY')`
   return (
-    <BaseText style={styles.title}>
-      Published {time}.{' '}
-      <BaseLink target="_blank" href="https://github.com/hundsim/js2019">
-        Source.
-      </BaseLink>
-    </BaseText>
+    <View style={styles.title}>
+      <BaseText style={{ marginBottom: 5 }}>Published {time}.</BaseText>
+      <BaseText>
+        <BaseLink href="mailto:henric.trotzig@gmail.com">Email.</BaseLink>{' '}
+        <BaseLink target="_blank" href="https://github.com/hundsim/js2019">
+          Source.
+        </BaseLink>{' '}
+      </BaseText>
+    </View>
   )
 }
 
