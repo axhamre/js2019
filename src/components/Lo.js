600fcc0c0c7fc5a95c50b275ba899679658af620
hundsim <hundsimma@gmail.com>
2019-01-14 11:03:36 +0100

    Update copy

--- src/components/Lo.js
+++ src/components/Lo.js
@@ -8,6 +8,7 @@ const siteLinks = [
   'https://skillbreak.com/',
   'https://calbird.com/',
   'https://www.habitud.se/pages/habitudpro',
+  'https://grundskolan.pfl.nu',
 ]
 
 export default () => (
@@ -16,6 +17,6 @@ export default () => (
     into a working product in no time{' '}
     {siteLinks.map((url, index) => renderLink({ url, index: index + 1 }))}.
     Full-stack with a good eye for the latest and greatest in Javascript (heard
-    of react-native-web? Lo has.). Ask him about headless CMS.
+    of react-native-web? Lo has.). Ask him about JAMstack.
   </Content>
 )
