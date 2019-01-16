da8c41c4919957807541783338a4ba37caf5510f
Lo <hundsimma@gmail.com>
2019-01-16 11:06:36 +0100

    Update copy

--- src/components/Lo.js
+++ src/components/Lo.js
@@ -2,21 +2,12 @@ import React from 'react'
 
 import UnderscoreText from './reusables/UnderscoreText'
 import Content from './Content'
-import { renderLink } from './Henric'
-
-const siteLinks = [
-  'https://skillbreak.com/',
-  'https://calbird.com/',
-  'https://www.habitud.se/pages/habitudpro',
-  'https://grundskolan.pfl.nu',
-]
 
 export default () => (
   <Content>
-    <UnderscoreText>Lo.</UnderscoreText> React-GraphQL wizard who can turn ideas
-    into a working product in no time{' '}
-    {siteLinks.map((url, index) => renderLink({ url, index: index + 1 }))}.
-    Full-stack with a good eye for the latest and greatest in Javascript (heard
-    of react-native-web? Lo has.). Ask him about JAMstack.
+    <UnderscoreText>Lo.</UnderscoreText> React wizard who can turn ideas into a
+    working product in no time. Full-stack with a good eye for the latest and
+    greatest in Javascript (heard of react-native-web? Lo has.). Ask him about
+    GraphQL.
   </Content>
 )
